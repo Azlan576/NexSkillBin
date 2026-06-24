c2ece12b47fc1a8754278cec7ed6f73db1b11637
Azlan576 <azlannahmad34@gmail.com>
2026-06-24 06:36:06 -0700

    my

---
+++ for tuples.py
@@ -0,0 +1,18 @@
+book_tuple=(445,"tink and grow rich","asim",50.5)
+
+print(book_tuple)
+
+print(type(book_tuple))
+
+print(len(book_tuple))
+
+for x in book_tuple:
+    print(x)
+
+print(book_tuple[2])
+
+print(type(book_tuple[2]))
+
+print(type(book_tuple[3]))
+
+
